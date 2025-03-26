5f808dceab78475b52c32406bf2de2df9b1097e7
soemin00 <dhakalramu07@gmail.com>
2025-03-27 02:51:22 +0530

    created the quiz.dart,start_screen.dart and question.dart then mapped the screens on clicking features

---
+++ lib/quiz.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/question_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(startQuiz: switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 58, 9, 143),
+                const Color.fromARGB(255, 121, 77, 196),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/start_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({required this.startQuiz, super.key});
+  final void Function() startQuiz;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 250,
+            color: const Color.fromARGB(200, 255, 255, 255),
+          ),
+          SizedBox(height: 80),
+          Text(
+            'Test Your Knowledge!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
+          SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {
+              startQuiz();
+            },
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: Icon(Icons.arrow_right_alt, color: Colors.white),
+            label: Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
