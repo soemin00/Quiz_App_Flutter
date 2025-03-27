289bd635fc6787f5761a6426f692a67239932fcf
soemin00 <dhakalramu07@gmail.com>
2025-03-27 16:36:06 +0530

    added a questions.dart file to store my questions and answer and updated the question_screen to fetch the data of questions in my question_screen and display the questions and answers, created a answer_button.dart file to make the elevated button widget and use it in the future whenever required

---
+++ lib/models/answer_button.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+        backgroundColor: const Color.fromARGB(255, 75, 8, 126),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/models/answer_button.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,6 +12,27 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Dummy Questions Here!!!');
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.text,
+            style: TextStyle(color: Colors.white, fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 60),
+          AnswerButton(answerText: currentQuestion.answers[0], onTap: () {}),
+          SizedBox(height: 20),
+          AnswerButton(answerText: currentQuestion.answers[1], onTap: () {}),
+          SizedBox(height: 20),
+          AnswerButton(answerText: currentQuestion.answers[2], onTap: () {}),
+          SizedBox(height: 20),
+          AnswerButton(answerText: currentQuestion.answers[3], onTap: () {}),
+        ],
+      ),
+    );
   }
 }
